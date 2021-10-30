cecfbc840af705700015e3602240ec69cd90527c
real-4w <real_4w@protonmail.com>
2021-10-30 15:10:51 +1300

    Working basic schedule

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Experiments with Python ATScheduler.
 
 Official library: https://github.com/real-4w/w_atscheduler.git
 Tutorial; https://betterprogramming.pub/introduction-to-apscheduler-86337f3bb4a6
+More info https://www.programcreek.com/python/example/94838/apscheduler.schedulers.background.BackgroundScheduler
--- w_atscheduler_tutorial.py
+++ w_atscheduler_tutorial.py
@@ -1,5 +1,27 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+import time,os 
+
+def tick():
+    print(f"Tick! The time is: {datetime.now()}.")
+    w_sched.print_jobs()
 
 w_sched = BackgroundScheduler(daemon=True)
 
-w_sched.add_job(lambda : w_sched.print_jobs(),'interval',seconds=5)
+
+w_sched.add_job(tick,'interval',seconds=5, id='w_sched')
+
+w_sched.start()
+print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+try:
+    # This is here to simulate application activity (which keeps the main thread alive).
+    while True:
+       time.sleep(10)
+except (KeyboardInterrupt, SystemExit):
+    # Not strictly necessary if daemonic mode is enabled but should be done if possible
+    w_sched.shutdown() 
+
+
+
+
